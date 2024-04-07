eab655c0450e7cf3106f910ce9b6df057ad625c4
unknown <ryanhenriq2019@gmail.com>
2024-04-07 17:21:33 -0300

    rota base de APIs para produção

--- src/constants/apiRoutes.js
+++ src/constants/apiRoutes.js
@@ -1,5 +1,8 @@
 /* FAZENDA */
-const API_BASE_ROUTE = 'http://localhost:3000/api';
+/* base route para desenvolvimento */
+//const API_BASE_ROUTE = 'http://localhost:3000/api';
+/* base route para produção */
+const API_BASE_ROUTE = 'https://irriga-agro.vercel.app/api';
 
 export const GET_FAZENDAS = `${API_BASE_ROUTE}/Farm/getFazendas`;
 export const POST_FAZENDAS = `${API_BASE_ROUTE}/Farm/PostFazenda`;
